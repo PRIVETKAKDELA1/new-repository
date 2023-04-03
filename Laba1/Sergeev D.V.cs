ea09296f1fdcdc37fea8f9b75099751ec02aab9e
PRIVETKAKDELA1 <s7rdanval@gmail.com>
2023-04-03 18:37:27 +0300

    first commit

---
+++ Laba1/Sergeev D.V.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Laba1
+{
+    class Program
+    {
+        static void Main( ) //объявление метода  
+        {
+            double A = 7, B = 13, C = 10, p, S; //переменные
+
+
+            p = ( A + B + C ) / 2;
+            S = Math. Sqrt( p * ( p - A ) * ( p - B ) * ( p - C) );
+            // Math.Sqrt - математическая функция которая извлекает квадратный корень
+
+            Console. WriteLine( "S = " + S );
+            Console. ReadLine( );
+        }
+    }
+}
